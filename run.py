3b4936e25ec4ab0b847d32fc8509a2930b33ed9e
yangmingxing <yangmingxing@zhexinit.com>
2018-12-22 18:22:23 +0800

    音频处理文件上传

---
+++ run.py
@@ -0,0 +1,51 @@
+# -*- coding:utf8 -*-
+
+from utools import wav_infos, read_wav, wav_to_pcm, pcm_to_wav, get_wav_time, get_ms_part_wav, get_second_part_wav, get_minute_part_wav
+
+wav_path = "./voice_files/demo.wav"
+pcm_path = "./voice_files/demo.pcm"
+wav_path2 = "./voice_files/test.wav"
+
+# 音频切割的文件路径
+main_wav_path = "./part_voice_files/12.wav"
+part_wav_path = "./part_voice_files/ms_part_voice.wav"
+second_part_wav_path = "./part_voice_files/second_part_wav_path.wav"
+minute_part_wav_path = "./part_voice_files/minute_part_wav_path.wav"
+
+# 获取音频信息
+ret = wav_infos(wav_path)
+print(ret)
+
+# 读取音频文件内容
+ret = read_wav(wav_path)
+print(ret)
+
+# 获取音频时长(单位秒)
+ret = get_wav_time(wav_path)
+print(ret)
+
+
+# 音频切片，获取部分音频 时间的单位是毫秒
+start_time = 13950
+end_time = 15200
+get_ms_part_wav(main_wav_path, start_time, end_time, part_wav_path)
+
+
+# 音频切片，获取部分音频 时间的单位是秒
+start_time = 35
+end_time = 38
+get_second_part_wav(main_wav_path, start_time, end_time, second_part_wav_path)
+
+# 音频切片，获取部分音频 时间的单位是分钟和秒 样式：0:12
+start_time = "0:35"
+end_time = "0:38"
+get_minute_part_wav(main_wav_path, start_time, end_time, minute_part_wav_path)
+
+
+# wav文件转为pcm文件
+wav_to_pcm(wav_path, pcm_path)
+
+# pcm文件转为wav文件
+pcm_to_wav(pcm_path, wav_path2)
+
+
---
+++ utools.py
@@ -0,0 +1,155 @@
+# -*- coding:utf8 -*-
+'''
+auth： Young
+公众号：Python疯子 （Hold2Crazy）
+'''
+import wave
+import contextlib
+import numpy as np
+
+from pydub import AudioSegment
+
+def wav_infos(wav_path):
+    '''
+    获取音频信息
+
+    :param wav_path: 音频路径
+    :return: [1, 2, 8000, 51158, 'NONE', 'not compressed']
+    对应关系：声道，采样宽度，帧速率，帧数，唯一标识，无损
+    '''
+    with wave.open(wav_path, "rb") as f:
+        f = wave.open(wav_path)
+
+        return list(f.getparams())
+
+
+
+
+def read_wav(wav_path):
+    '''
+    读取音频文件内容:只能读取单声道的音频文件, 这个比较耗时
+
+    :param wav_path: 音频路径
+    :return:  音频内容
+    '''
+    with wave.open(wav_path, "rb") as f:
+        # 读取格式信息
+        # 一次性返回所有的WAV文件的格式信息，它返回的是一个组元(tuple)：声道数, 量化位数（byte单位）, 采
+        # 样频率, 采样点数, 压缩类型, 压缩类型的描述。wave模块只支持非压缩的数据，因此可以忽略最后两个信息
+        params = f.getparams()
+        nchannels, sampwidth, framerate, nframes = params[:4]
+
+        # 读取声音数据，传递一个参数指定需要读取的长度（以取样点为单位）
+        str_data = f.readframes(nframes)
+
+    return str_data
+
+def get_wav_time(wav_path):
+    '''
+    获取音频文件是时长
+
+    :param wav_path: 音频路径
+    :return: 音频时长 (单位秒)
+    '''
+    with contextlib.closing(wave.open(wav_path, 'r')) as f:
+        frames = f.getnframes()
+    rate = f.getframerate()
+    duration = frames / float(rate)
+    return duration
+
+
+def get_ms_part_wav(main_wav_path, start_time, end_time, part_wav_path):
+    '''
+    音频切片，获取部分音频 单位是毫秒级别
+
+    :param main_wav_path: 原音频文件路径
+    :param start_time:  截取的开始时间
+    :param end_time:  截取的结束时间
+    :param part_wav_path:  截取后的音频路径
+    :return:
+    '''
+    start_time = int(start_time)
+    end_time = int(end_time)
+
+    sound = AudioSegment.from_mp3(main_wav_path)
+    word = sound[start_time:end_time]
+
+    word.export(part_wav_path, format="wav")
+
+
+def get_second_part_wav(main_wav_path, start_time, end_time, part_wav_path):
+    '''
+    音频切片，获取部分音频 单位是秒级别
+
+    :param main_wav_path: 原音频文件路径
+    :param start_time:  截取的开始时间
+    :param end_time:  截取的结束时间
+    :param part_wav_path:  截取后的音频路径
+    :return:
+    '''
+    start_time = int(start_time) * 1000
+    end_time = int(end_time) * 1000
+
+    sound = AudioSegment.from_mp3(main_wav_path)
+    word = sound[start_time:end_time]
+
+    word.export(part_wav_path, format="wav")
+
+def get_minute_part_wav(main_wav_path, start_time, end_time, part_wav_path):
+    '''
+    音频切片，获取部分音频 分钟:秒数  时间样式："12:35"
+
+    :param main_wav_path: 原音频文件路径
+    :param start_time:  截取的开始时间
+    :param end_time:  截取的结束时间
+    :param part_wav_path:  截取后的音频路径
+    :return:
+    '''
+
+    start_time = (int(start_time.split(':')[0])*60+int(start_time.split(':')[1]))*1000
+    end_time = (int(end_time.split(':')[0])*60+int(end_time.split(':')[1]))*1000
+
+    sound = AudioSegment.from_mp3(main_wav_path)
+    word = sound[start_time:end_time]
+
+    word.export(part_wav_path, format="wav")
+
+
+def wav_to_pcm(wav_path, pcm_path):
+    '''
+    wav文件转为pcm文件
+
+    :param wav_path:wav文件路径
+    :param pcm_path:要存储的pcm文件路径
+    :return: 返回结果
+    '''
+    f = open(wav_path, "rb")
+    f.seek(0)
+    f.read(44)
+
+    data = np.fromfile(f, dtype=np.int16)
+    data.tofile(pcm_path)
+
+def pcm_to_wav(pcm_path, wav_path):
+    '''
+    pcm文件转为wav文件
+
+    :param pcm_path: pcm文件路径
+    :param wav_path: wav文件路径
+    :return:
+    '''
+    f = open(pcm_path,'rb')
+    str_data  = f.read()
+    wave_out=wave.open(wav_path,'wb')
+    wave_out.setnchannels(1)
+    wave_out.setsampwidth(2)
+    wave_out.setframerate(8000)
+    wave_out.writeframes(str_data)
+
+
+
+
+
+
+
+
